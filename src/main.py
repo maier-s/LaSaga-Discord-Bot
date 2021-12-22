dc11dfd4b0632280aa7b2e5116c6a9d1c551b9db
Stefan Maier <Stefan.Maier95tt@web.de>
2021-12-22 22:13:50 +0100

    makle TOKEN globally available in python file

--- src/main.py
+++ src/main.py
@@ -2,16 +2,24 @@ import os
 from LaSaga_Bot import LaSaga_Bot
 from dotenv import load_dotenv
 
+TOKEN = None
+GUILD = None
+
 def setup():
+    global TOKEN
+    global GUILD
+
     load_dotenv()
+    
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
 
 if __name__ == "__main__":
     #setup environment
     setup()
 
     #load neccessary data
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    GUILD = os.getenv('DISCORD_GUILD')
+    
 
     # Create Discord Bot Class and Run it
 
