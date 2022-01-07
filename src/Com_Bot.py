c1b93b6066ae4486f4abbbe0db1eae0e9c8c2d3a
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2022-01-07 01:17:31 +0100

    Added W2G Command
    add to Playlist currently not functional

--- src/Com_Bot.py
+++ src/Com_Bot.py
@@ -1,4 +1,11 @@
 from discord.ext import commands
+from discord.ext.commands.core import command
+from W2G import W2G
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+W2G_TOKEN = os.getenv('W2G_TOKEN')
 
 @commands.command(name="test")
 async def test(ctx,args):
@@ -7,3 +14,21 @@ async def test(ctx,args):
 @commands.command(name="fu")
 async def fu(ctx):
         await ctx.send("You too!")
+@commands.command(name="w2g")
+async def w2g(ctx, arg1, arg2,arg3 = None):
+        W2G_Handler = W2G(W2G_TOKEN)
+        if arg1 == "create":
+                link,streamkey = W2G_Handler.makeRoom(arg2)
+                await ctx.send(f'Your Streamkey is: {streamkey} \nHere is your Room: {link}')
+        elif arg1  == "update":
+                if W2G_Handler.updateRoom(arg2,arg3):
+                        await ctx.send(f'Update sucessfull on Room: {arg2}')
+                        return
+                else:
+                        await ctx.send(f'Somthing went wrong using the following information to update room \nStreamkey:{arg2}\nURL:{arg3}')
+        elif arg1 == "add":
+                if W2G_Handler.addtoPlaylist(arg2,arg3):
+                        await ctx.send(f'Add Video sucessfull to Room: {arg2}')
+                        return
+                else:
+                        await ctx.send(f'Somthing went wrong using the following information to add a video \nStreamkey:{arg2}\nURL:{arg3}')
--- src/W2G.py
+++ src/W2G.py
@@ -3,28 +3,46 @@ from dotenv import load_dotenv
 import os
 class W2G:
     W2G_TOKEN = None
-    URL = "https://w2g.tv/rooms/create.json"
+    URL_CREATE = "https://w2g.tv/rooms/create.json"
     def __init__(self,Token=None) -> None:
-        print("Congratulation you use Watch2gether Class from LaSaga Bot")
         if Token == None:
             print("No Token assigend")
             self.__del__()
         else:
             self.W2G_TOKEN = Token
 
-    def makeRoom(self,url:str)->str:
+    def makeRoom(self,url:str)->(str,str):
         if url == None:
             return None # make error handling
         req = {
                        'w2g_api_key': self.W2G_TOKEN, 
-                       'share': url,
+                       'share': url
             }
-        response = requests.post(url=self.URL,data=headers).json()['streamkey']
-        return f'https://w2g.tv/rooms/{response}'
-
-if __name__ == "__main__":
-    load_dotenv()
-    W2G_TOKEN = os.getenv('W2G_TOKEN')
-    test = W2G(Token=W2G_TOKEN)
-    test.getRoom()
-    print("Hallo Welt")
+        response = requests.post(url=self.URL_CREATE,data=req).json()['streamkey']
+        return (f'https://w2g.tv/rooms/{response}',response)
+    def updateRoom(self,streamkey:str,url:str)->bool:
+        if url == None or streamkey == None:
+            return False
+        req = {
+                       'w2g_api_key': self.W2G_TOKEN, 
+                       'item_url': url
+            }
+        response = requests.post(url=f'https://w2g.tv/rooms/{streamkey}/sync_update',data=req)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    def addtoPlaylist(self,streamkey:str,url:str)->bool:
+        if url == None or streamkey == None:
+            return False
+        req = {
+                       "w2g_api_key": self.W2G_TOKEN,
+                        "add_items": [{"url": url,"title": "Hello World"}]
+            }
+        print(req)
+        response = requests.post(url=f'https://w2g.tv/rooms/{streamkey}/playlists/current/playlist_items/sync_update',data=req)
+        print(response)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
